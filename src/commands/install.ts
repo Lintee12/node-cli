539047ba3d402b8c64c153dc637ce45cb01af5dc
Evan <lintonevan@gmail.com>
2025-02-17 22:54:37 -0400

    fix: removed logs

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -15,9 +15,7 @@ export async function install(args?: string[]) {
   }
 
   try {
-    console.log(fullFilePath);
     const importedModule = await import(`${fullFilePath}`);
-    console.log(importedModule);
     const newCommand = importedModule.default || importedModule;
 
     if (newCommand.command && newCommand.description && typeof newCommand.callback === "function") {
